69334e6458b8b50357b99d67ec3787dd50ee4be3
user.name: "Asrar Firdoos <user.email: "theasrarfirdous@gmail.com>
2026-08-04 23:37:50 +0530

    fix: vite.config.js updated and removed npmrc

--- frontend/weather/vite.config.js
+++ frontend/weather/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // Tells Vite 8 to bypass lightningcss and use esbuild instead
+    cssMinify: 'esbuild'
+  }
 })
