c93a2d0e8cb900215f85297a91115bcc4fa5af2c
user.name: "Asrar Firdoos <user.email: "theasrarfirdous@gmail.com>
2026-08-04 23:56:29 +0530

    fix: lock-file removed config modify vite

--- frontend/weather/vite.config.js
+++ frontend/weather/vite.config.js
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  css: {
+    // Force Vite 8 to completely drop lightningcss parsing
+    transformer: 'postcss' 
+  },
   build: {
     // Tells Vite 8 to bypass lightningcss and use esbuild instead
     cssMinify: 'esbuild'
